2590a6c19cd1991b96aad30f9cc61bb5c864ee5b
Jan Převrátil <honzaprevratil@seznam.cz>
2019-01-31 08:04:35 +0100

    Přidejte soubory projektu.

---
+++ CodeShare-NETCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeShare_NETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
